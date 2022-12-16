f75455613a60aeedd19b31f85a33c9128ee9ff1d
penafernando <fernando.pena@pomelo.la>
2022-12-16 16:49:42 -0300

    Fix Scene init

--- PomeloCardsDemo/SceneDelegate.swift
+++ PomeloCardsDemo/SceneDelegate.swift
@@ -15,18 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-                
-        self.window = UIWindow(windowScene: windowScene)
-        setupWindow()
         setupPomeloCards()
     }
 
-    private func setupWindow() {
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
-        window?.makeKeyAndVisible()
-    }
-    
     private func setupPomeloCards() {
         //Configure Cards SDK
         PomeloCards.initialize(with: PomeloCardsConfiguration(environment: .staging))
