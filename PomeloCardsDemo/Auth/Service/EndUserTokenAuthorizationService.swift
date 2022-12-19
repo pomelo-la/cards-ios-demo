6d63a2fffade5e1add353f681dd367f58ba1566b
penafernando <fernando.pena@pomelo.la>
2022-12-19 16:57:40 -0300

    Create a `FunctionConverter` helper to handle async to sync logic

--- PomeloCardsDemo/Auth/Service/EndUserTokenAuthorizationService.swift
+++ PomeloCardsDemo/Auth/Service/EndUserTokenAuthorizationService.swift
@@ -21,38 +21,17 @@ class EndUserTokenAuthorizationService: PomeloAuthorizationServiceProtocol {
         self.endUserTokenResolver = endUserTokenResolver
     }
     
-    /// Function that returns a valid end user token. This function is sync, usually this token is provided by a backend asynchronously. Here is an example of how you can implement it by using a `DispatchGroup`
+    /// Function that returns a valid end user token. This function is sync, usually this token is provided by a backend asynchronously. Here is an example of how you can implement it by using out `FunctionConverter`
     /// - Returns: End user token
     func getValidToken() -> String {
-        var token = ""
-        let group = DispatchGroup()
-
-        group.enter()
-        
-        DispatchQueue.global(qos: .default).async { [weak self] in
-            self?.getUserToken { endUserToken in
-            
-                guard let endUserToken = endUserToken else {
-                    print("Cards Sample App error!🔥 Cannot obtain client token")
-                    group.leave()
-                    return
-                }
-
-                token = endUserToken
-                group.leave()
-            }
-        }
-        
-        group.wait()
-        return token
+        return FunctionConverter.asyncToSync(function: getUserToken) ?? ""
     }
     
     
     /// Funtion that gets a valid token from a service asynchronously
     /// - Parameter completion: A closure that returns a end user token if it was able to retrieve one
     private func getUserToken(completion: @escaping (String?) -> Void) {
-        guard let userEmail = self.emailProvider.provideCurrentUserEmail()
-        else {
+        guard let userEmail = self.emailProvider.provideCurrentUserEmail() else {
             print("Cards Sample App error!🔥 - UserTokenService: Couldn't get user email")
             completion(nil)
             return
@@ -63,7 +42,6 @@ class EndUserTokenAuthorizationService: PomeloAuthorizationServiceProtocol {
                 completion(nil)
                 return
             }
-
             completion(endUserToken)
         }
     }
