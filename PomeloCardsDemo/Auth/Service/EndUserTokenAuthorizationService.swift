abe6eb52588ae8781298078ad3165ee92c90b6d7
penafernando <fernando.pena@pomelo.la>
2022-12-19 14:57:02 -0300

    Add documentation in code to local auth handler

--- PomeloCardsDemo/Auth/Service/EndUserTokenAuthorizationService.swift
+++ PomeloCardsDemo/Auth/Service/EndUserTokenAuthorizationService.swift
@@ -1,5 +1,5 @@
 //
-//  CardsAuthorizationService.swift
+//  EndUserTokenAuthorizationService.swift
 //  CardsSampleApp
 //
 //  Created by Oscar Odon on 08/08/2022.
@@ -9,6 +9,8 @@ import Foundation
 import PomeloCards
 import PomeloNetworking
 
+
+/// Client implementation of `PomeloAuthorizationServiceProtocol`. It must return a valid end user token for a given client and user by implementing `func getValidToken() -> String`
 class EndUserTokenAuthorizationService: PomeloAuthorizationServiceProtocol {
     private let emailProvider: UserEmailProviderProtocol
     private let endUserTokenResolver: UserEmailEndUserTokenResolverProtocol
@@ -19,6 +21,8 @@ class EndUserTokenAuthorizationService: PomeloAuthorizationServiceProtocol {
         self.endUserTokenResolver = endUserTokenResolver
     }
     
+    /// Function that returns a valid end user token. This function is sync, usually this token is provided by a backend asynchronously. Here is an example of how you can implement it by using a `DispatchGroup`
+    /// - Returns: End user token
     func getValidToken() -> String {
         var token = ""
         let group = DispatchGroup()
@@ -43,6 +47,9 @@ class EndUserTokenAuthorizationService: PomeloAuthorizationServiceProtocol {
         return token
     }
     
+    
+    /// Funtion that gets a valid token from a service asynchronously
+    /// - Parameter completion: A closure that returns a end user token if it was able to retrieve one
     private func getUserToken(completion: @escaping (String?) -> Void) {
         guard let userEmail = self.emailProvider.provideCurrentUserEmail()
         else {
