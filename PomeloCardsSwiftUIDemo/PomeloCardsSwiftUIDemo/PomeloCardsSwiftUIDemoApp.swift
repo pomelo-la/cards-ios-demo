6fd4f59488fcf502da02cf8773cdf0dccc90a542
penafernando <fernando.pena@pomelo.la>
2023-02-23 12:53:50 -0300

    Create  project

---
+++ PomeloCardsSwiftUIDemo/PomeloCardsSwiftUIDemo/PomeloCardsSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomeloCardsSwiftUIDemoApp.swift
+//  PomeloCardsSwiftUIDemo
+//
+//  Created by Fernando Pena on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PomeloCardsSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
