419b802b6967e62ce66ee3ef8348f1619cde8779
penafernando <fernando.pena@pomelo.la>
2023-03-01 17:34:42 -0300

    Add vscode debugger. Update App screen to a list.

--- PomeloCardsReactNativeDemo/App.js
+++ PomeloCardsReactNativeDemo/App.js
@@ -5,7 +5,38 @@
  * @format
  */
 import React from 'react';
-import { Button, SafeAreaView, NativeModules } from 'react-native';
+import { SafeAreaView, NativeModules, StyleSheet, StatusBar, View, SectionList, TouchableOpacity, Text } from 'react-native';
+
+const DATA = [
+  {
+    title: 'SDK',
+    data: [{
+      id: 'cards-sdk',
+      title: 'Cards',
+    }]
+  },
+  {
+    title: 'Widgets',
+    data: [
+      {
+        id: 'widget-card',
+        title: 'Card',
+      },
+      {
+        id: 'widget-card-list',
+        title: 'Card list',
+      },
+      {
+        id: 'widget-change-pin',
+        title: 'Change Pin',
+      },
+      {
+        id: 'widget-activate-card',
+        title: 'Activate Card',
+      }
+    ]
+  }
+];
 
 const App = () => {
   function launchCards() {
@@ -16,16 +47,59 @@ const App = () => {
     }) 
   }
 
+  function onPressItem(item) {
+    if (item.id == 'cards-sdk') {
+      launchCards()
+    } else  {
+      console.log(item)
+    }
+  }
+
+  const Item = ({item}) => (
+    <TouchableOpacity onPress={ () => onPressItem(item) }>
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+    </View>
+    </TouchableOpacity>
+  );
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Button
-        title = "Launch Cards"
-        onPress = {() => 
-          launchCards()
-        }
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <Item item={item} />}
+        renderSectionHeader={({section: {title}}) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
       />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  header: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  title: {
+    fontSize: 16,
+  },
+});
+
 
 export default App;
